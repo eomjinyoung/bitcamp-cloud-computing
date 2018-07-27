920c74deb292f3c9e9f242e6c083636b6148711c
eomjinyoung <jinyoung.eom@gmail.com>
2018-07-27 17:01:38 +0900

    .

---
+++ bitcamp-web-18/src/main/webapp/html/js/common.js
@@ -0,0 +1,13 @@
+function parseQuery(url) {
+    var paramMap = {};
+
+    var qs = url.split('?');
+    if (qs.length > 1) {
+        var params = qs[1].split('&');
+        for (var param of params) {
+            var kv = param.split('=')
+            paramMap[kv[0]] = kv[1];
+        }
+    }
+    return paramMap;
+}
