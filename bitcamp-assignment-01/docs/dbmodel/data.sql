6d0db4413eb3bda9f666575c58fa321f88a70109
eomjinyoung <jinyoung.eom@gmail.com>
2018-08-20 10:12:25 +0900

    .

---
+++ bitcamp-assignment-01/docs/dbmodel/data.sql
@@ -0,0 +1,15 @@
+-- 회원 정보 입력 
+insert into P2_MEMB(MNO,EMAIL,NAME,PWD)
+values(1, 'user01@test.com', 'user01', password('1111'));
+
+insert into P2_MEMB(MNO,EMAIL,NAME,PWD)
+values(1, 'user02@test.com', 'user02', password('1111'));
+
+insert into P2_MEMB(MNO,EMAIL,NAME,PWD)
+values(1, 'user03@test.com', 'user03', password('1111'));
+
+insert into P2_MEMB(MNO,EMAIL,NAME,PWD)
+values(1, 'user04@test.com', 'user04', password('1111'));
+
+insert into P2_MEMB(MNO,EMAIL,NAME,PWD)
+values(1, 'user05@test.com', 'user05', password('1111'));
